7b3edee75496f2df7c4049124a033a91bea1f92a
Nikho311 <90280360+Nikhoo311@users.noreply.github.com>
2025-09-29 14:02:16 +0200

    feature/addFirebase (#1)
    feat: add firebase to the project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDTrKRK2WQge9sDdaZ_l_Pdv8A7HgKwkO0",
+    authDomain: "remember-me-e9313.firebaseapp.com",
+    projectId: "remember-me-e9313",
+    storageBucket: "remember-me-e9313.firebasestorage.app",
+    messagingSenderId: "161787533144",
+    appId: "1:161787533144:web:e24462b05a7a144287772d"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
